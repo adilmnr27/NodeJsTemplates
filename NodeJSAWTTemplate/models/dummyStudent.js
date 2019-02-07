02728bb595cd867eb0cf9b9fba0bb57b4c739b97
unknown <adilmnr27@gmail.com>
2019-02-07 20:19:43 +0530

    minor bug

--- NodeJSAWTTemplate/models/dummyStudent.js
+++ NodeJSAWTTemplate/models/dummyStudent.js
@@ -4,12 +4,12 @@ module.exports = {
     firstName: 'Jon',
     lastName: 'Doe',
     dob: '12/11/1991',
-    role:'student',
+    role: 'student',
     email: 'user@gmail.com',
     address: {
         street: '555 Bayshore Blvd',
         city: 'Tampa',
         state: 'Florida',
-        zip: '33813' 
+        zip: '33813'
     }
 };
