13315f866744809fad9e30d02ec6071557eddf68
unknown <adilmnr27@gmail.com>
2019-02-11 20:10:42 +0530

    Inital logger commit

---
+++ NodeJSLogger/routes/studentRoutes.js
@@ -0,0 +1,17 @@
+/*-------------For Student Routes ie. routes which start with /api/student---------*/
+"use strict";
+const express = require('express')
+const router = express.Router();
+
+//indirectly the url is portNumber:/api/student/login
+router.get('/login', (req, res) => {
+    res.send("Student logged in")
+});
+
+//indirectly the url is portNumber:/api/student/
+router.get('/', (req, res) => {
+    res.send("student view");
+})
+
+//exporting the router object so that we can import it in app.js
+module.exports = router;
